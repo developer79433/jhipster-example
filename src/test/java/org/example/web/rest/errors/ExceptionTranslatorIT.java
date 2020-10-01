e36b940d233a0e48c413577cc597d45fa8320f0e
Matt <fhneahnfcekhnfcerinhfgcirek@mailinator.com>
2020-10-01 11:44:48 +1000

    Remove Spring Security.

--- src/test/java/org/example/web/rest/errors/ExceptionTranslatorIT.java
+++ src/test/java/org/example/web/rest/errors/ExceptionTranslatorIT.java
@@ -4,7 +4,6 @@ import org.example.JhipsterexampleApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,7 +12,6 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 /**
  * Integration tests {@link ExceptionTranslator} controller advice.
  */
-@WithMockUser
 @AutoConfigureWebTestClient
 @SpringBootTest(classes = JhipsterexampleApp.class)
 public class ExceptionTranslatorIT {
